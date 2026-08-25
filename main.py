b5c17e1dae4b66ef38e1d9710d13733b3f1085d5
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-08-25 16:59:10 +0300

    Refactor Post model to include default value for published attribute

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 
 from fastapi import FastAPI
 from fastapi.params import Body
-from pydantic import BaseModel  # noqa: F401
+from pydantic import BaseModel
 
 app = FastAPI()
 
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
 
 @app.get("/")
 async def root():           
