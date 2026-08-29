97a18ff7c34faece859cb4d1f05240a88582135e
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-08-30 02:28:24 +0300

    Enhance test_post endpoint to return all posts and update create_posts to use database session

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-# 4:41:00
+# 5:04:00
 
 import os
 import time
@@ -74,14 +74,18 @@ async def get_posts():
 
 @app.get("/sql")
 async def test_post(db: Session = Depends(get_db)):  # noqa: B008
-    return {"Status" : "Success"}
+
+    posts = db.query(models.Post).all()
+    return {"Data" : posts}
 
 # -------------------------------------------------------------------------
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_posts(post: Post):
+async def create_posts(post: Post, db: Session = Depends(get_db)):  # noqa: B008
 
     cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
+
+    db.query(models.Post).
 
     new_post = cursor.fetchone()
     connect.commit()
