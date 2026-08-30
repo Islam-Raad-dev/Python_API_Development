ae5aee1784813e9ba2195446c80172e2ab61a885
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-08-30 15:30:57 +0300

    Remove id and created_at fields from UserCreate schema

--- app/schemas.py
+++ app/schemas.py
@@ -21,9 +21,7 @@ class Config:
     orm_mode = True
 
 class UserCreate(BaseModel):
-    id : int
     first_name : str
     last_name: str
     email: EmailStr
     password: str
-    created_at : datetime
