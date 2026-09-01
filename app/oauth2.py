2d130fbcdbf42a55b9ccc931fe0bedf759719064
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-09-01 18:39:28 +0300

    Refactor oauth2.py to enhance token handling and user verification logic

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,7 +1,13 @@
 import os
 from datetime import datetime, timedelta, timezone
 
-from jose import JWTError, jwt  # noqa: F401
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+
+from . import schemas
+
+oauth2_schemas = OAuth2PasswordBearer(tokenUrl="login")
 
 #SECRET_KEY
 #ALGORITHM
@@ -10,6 +16,8 @@ from jose import JWTError, jwt  # noqa: F401
 SECRET_KEY = os.getenv("SECRET_KEY_FRO_JWT", "fallback_secret_key_for_dev_only")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# -------------------------------------------------------------------------
 
 
 def create_access_token(data: dict):
@@ -21,3 +29,30 @@ def create_access_token(data: dict):
     to_encode.update({"exp" : expire})
 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+# -------------------------------------------------------------------------
+
+def verify_access_token(token: str, credentials_exception):
+
+    try:
+        payload = jwt.decode(token=token, key=SECRET_KEY, algorithms=ALGORITHM)
+
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+
+        token_data = schemas.TokenData(id=id)  # noqa: F841
+
+    except JWTError:
+        raise credentials_exception
+
+# -------------------------------------------------------------------------
+
+def get_current_user(token: str = Depends(oauth2_schemas)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="could not validate crecredentials ", headers={"WWW-Authenticate" : "Bearer"}
+    )
+
+    return verify_access_token(token=token, credentials_exception=credentials_exception)
