c6ac9a1cb4966f4f20ea6f0fb47e2fafd77b74ac
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-09-01 11:27:09 +0300

    Fix access token creation by removing redundant variable assignment

--- app/oauth2.py
+++ app/oauth2.py
@@ -17,8 +17,7 @@ def create_access_token(data: dict):
     to_encode = data.copy()
 
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
     to_encode.update({"exp" : expire})
 
-    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-    return encode_jwt
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
