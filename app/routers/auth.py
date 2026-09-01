7315cecacae35c10fb8261d0fde40384181864a8
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-09-01 12:12:06 +0300

    Update login_user to use OAuth2PasswordRequestForm and include token_type in response

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,6 +5,7 @@ from fastapi import (  # noqa: F401
     Response,
     status,
 )
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from .. import database, models, oauth2, schemas, utils
@@ -22,7 +23,7 @@ router = APIRouter(tags=["Authentication"])
 
 @router.post("/login", status_code=status.HTTP_200_OK)
 
-async def login_user(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):  # noqa: B008
+async def login_user(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):  # noqa: B008
 
     user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
 
@@ -41,4 +42,4 @@ async def login_user(user_credentials: schemas.UserLogin, db: Session = Depends(
 
     access_token = oauth2.create_access_token(data={"user_id": user.id})
 
-    return {"access_token" : access_token}
+    return {"access_token" : access_token, "token_type" : "bearer"}
