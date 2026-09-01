f34ecae6bd21e94669768c216deddd277d310810
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-09-01 09:38:06 +0300

    Implement user login endpoint and update schemas for user authentication

---
+++ app/routers/auth.py
@@ -0,0 +1,35 @@
+from fastapi import (  # noqa: F401
+    APIRouter,
+    Depends,
+    HTTPException,
+    Response,
+    status,
+)
+from sqlalchemy.orm import Session
+
+from .. import database, models, schemas, utils
+
+# -------------------------------------------------------------------------
+
+models.Base.metadata.create_all(bind=database.engine)
+
+# -------------------------------------------------------------------------
+
+router = APIRouter(tags=["Authentication"])
+
+# -------------------------------------------------------------------------
+
+
+@router.post("/login", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+
+async def login_user(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):  # noqa: B008
+
+    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"user with email: {user_credentials.email} was not found",
+        )
+
+    return user
