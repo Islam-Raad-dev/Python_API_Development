71485656fe6b25b8f6ddfba55583d3f71d095ce5
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-09-02 18:01:03 +0300

    Fix algorithm parameter type in create_access_token and update TokenData id type to support int

--- backend/app/oauth2.py
+++ backend/app/oauth2.py
@@ -28,7 +28,7 @@ def create_access_token(data: dict):
 
     to_encode.update({"exp" : expire})
 
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=[ALGORITHM])
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 # -------------------------------------------------------------------------
 
