388b554cbaa896ca285d6b994acb124f4735d05a
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-09-04 17:20:31 +0300

    Fix user_id reference in vote functionality to use current_user.id

--- backend/app/routers/vote.py
+++ backend/app/routers/vote.py
@@ -26,7 +26,7 @@ async def vote(vote: schemas.Vote,
                 current_user:int = Depends(oauth2.get_current_user)):
 
     vote_query = db.query(models.Vote).filter(models.Vote.post_id ==   
-    vote.post_id,models.Vote.user_id == vote.user_id)
+    vote.post_id,models.Vote.user_id == current_user.id)
 
     found_vote = vote_query.first()
 
@@ -36,7 +36,7 @@ async def vote(vote: schemas.Vote,
                 status_code=status.HTTP_409_CONFLICT,
                 detail=f"user {current_user.id} has already vote on post {vote.post_id}")
         
-        new_vote = models.Vote(post_id = vote.post_id, user_id = vote.user_id)
+        new_vote = models.Vote(post_id = vote.post_id, user_id = current_user.id)
     
         db.add(new_vote)
         db.commit()
