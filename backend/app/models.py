4bdc17dee7b971d3e97a8005925ab1c943f94c63
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-09-04 15:30:01 +0300

    Refactor Vote model to remove unnecessary fields and retain only user and post relationships

--- backend/app/models.py
+++ backend/app/models.py
@@ -35,13 +35,5 @@ class User(Base):
 class Vote(Base):
     __tablename__ = "votes"
 
-    id = Column(Integer, primary_key=True, nullable=False)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False)
-    created_at = Column(
-        TIMESTAMP(timezone=True),
-        nullable=False,
-        server_default=text("CURRENT_TIMESTAMP"),
-    )
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
