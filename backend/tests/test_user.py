aab6975278f0e64ebd031084afbebbdd05f4e162
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-09-14 18:20:48 +0300

    Add user creation and login tests with parameterized incorrect login scenarios

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -1,5 +1,11 @@
+import os
+
+import pytest
+from app import schemas
 from app.main import app
+from app.oauth2 import algorithm
 from fastapi.testclient import TestClient
+from jose import jwt
 
 client = TestClient(app)
 
@@ -7,3 +13,41 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to the FastAPI application!"}
+
+
+def test_create_user(client):
+    res = client.post(
+        "/users/", json={"email": "hello123@gmail.com", "password": "password123"})
+
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello123@gmail.com"
+    assert res.status_code == 201
+
+SECRET_KEY_FOR_JWT = os.getenv("SECRET_KEY_FOR_JWT")
+
+
+def test_login_user(test_user, client):
+    res = client.post(
+        "/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token,
+                         SECRET_KEY_FOR_JWT, algorithms=[algorithm])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('islam@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('islam@gmail.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post(
+        "/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
