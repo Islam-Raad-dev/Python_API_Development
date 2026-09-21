7d692f7f4efe72fd5db17610b63782bd746ea33f
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-09-21 10:54:34 +0300

    Refactor user tests: improve readability and structure of test_create_user and test_login_user

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -9,6 +9,8 @@ from jose import jwt
 
 client = TestClient(app)
 
+SECRET_KEY_FOR_JWT = os.getenv("SECRET_KEY_FOR_JWT")
+
 
 def test_root():
     response = client.get("/")
@@ -16,45 +18,48 @@ def test_root():
     assert response.json() == {"message": "Welcome to the FastAPI application!"}
 
 
-
 def test_create_user(client):
     res = client.post(
-        "/users/", json={"email": "hello123@gmail.com", "password": "password123"})
+        "/users/",
+        json={
+            "first_name": "Test",
+            "last_name": "User",
+            "email": "hello123@gmail.com",
+            "password": "password123",
+        },
+    )
 
+    assert res.status_code == 201
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "hello123@gmail.com"
-    assert res.status_code == 201
-
-
-SECRET_KEY_FOR_JWT = os.getenv("SECRET_KEY_FOR_JWT")
-
 
 
 def test_login_user(test_user, client):
     res = client.post(
-        "/login", data={"username": test_user['email'], "password": test_user['password']})
-    login_res = schemas.Token(**res.json())
-    payload = jwt.decode(login_res.access_token,
-                         SECRET_KEY_FOR_JWT, algorithms=[ALGORITHM])
-    id = payload.get("user_id")
-    assert id == test_user['id']
-    assert login_res.token_type == "bearer"
+        "/login",
+        data={"username": test_user["email"], "password": test_user["password"]},
+    )
     assert res.status_code == 200
 
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(
+        login_res.access_token, SECRET_KEY_FOR_JWT, algorithms=[ALGORITHM]
+    )
+    id = payload.get("user_id")
+    assert id == test_user["id"]
+    assert login_res.token_type == "bearer"
 
 
-@pytest.mark.parametrize("email, password, status_code", [
-    ('wrongemail@gmail.com', 'password123', 403),
-    ('islam@gmail.com', 'wrongpassword', 403),
-    ('wrongemail@gmail.com', 'wrongpassword', 403),
-    (None, 'password123', 422),
-    ('islam@gmail.com', None, 422)
-])
-
-
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("wrongemail@gmail.com", "password123", 403),
+        ("islam@gmail.com", "wrongpassword", 403),
+        ("wrongemail@gmail.com", "wrongpassword", 403),
+        (None, "password123", 422),
+        ("islam@gmail.com", None, 422),
+    ],
+)
 def test_incorrect_login(test_user, client, email, password, status_code):
-    res = client.post(
-        "/login", data={"username": email, "password": password})
-
+    res = client.post("/login", data={"username": email, "password": password})
     assert res.status_code == status_code
-    # assert res.json().get('detail') == 'Invalid Credentials'
