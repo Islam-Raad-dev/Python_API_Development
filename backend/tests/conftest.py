9c6eddca29cc0b7aa39d00299ed0930cd6062d05
Islam-Raad-dev <islamraad.dev@gmail.com>
2026-09-21 10:54:23 +0300

    Refactor test fixtures: streamline user creation and improve session management

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -9,13 +9,11 @@ from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 @pytest.fixture()
 def session():
-    print("my session fixture ran")
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
     db = TestingSessionLocal()
@@ -28,86 +26,92 @@ def session():
 @pytest.fixture()
 def client(session):
     def override_get_db():
-
         try:
             yield session
         finally:
             session.close()
+
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
 
 
 @pytest.fixture
-def test_user2(client):
-    user_data = {"email": "islam@gmail.com",
-                 "password": "11223344"}
+def test_user(client):
+    user_data = {
+        "first_name": "Islam",
+        "last_name": "Raad",
+        "email": "islam@gmail.com",
+        "password": "11223344",
+    }
     res = client.post("/users/", json=user_data)
-
     assert res.status_code == 201
 
     new_user = res.json()
-    new_user['password'] = user_data['password']
+    new_user["password"] = user_data["password"]
     return new_user
 
 
 @pytest.fixture
-def test_user(client):
-    user_data = {"email": "islam@gmail.com",
-                 "password": "11223344"}
+def test_user2(client):
+    user_data = {
+        "first_name": "Second",
+        "last_name": "User",
+        "email": "second_user@gmail.com",
+        "password": "11223344",
+    }
     res = client.post("/users/", json=user_data)
-
     assert res.status_code == 201
 
     new_user = res.json()
-    new_user['password'] = user_data['password']
+    new_user["password"] = user_data["password"]
     return new_user
 
 
 @pytest.fixture
 def token(test_user):
-    return create_access_token({"user_id": test_user['id']})
+    return create_access_token({"user_id": test_user["id"]})
 
 
 @pytest.fixture
 def authorized_client(client, token):
     client.headers = {
         **client.headers,
-        "Authorization": f"Bearer {token}"
+        "Authorization": f"Bearer {token}",
     }
-
     return client
 
 
 @pytest.fixture
 def test_posts(test_user, session, test_user2):
-    posts_data = [{
-        "title": "first title",
-        "content": "first content",
-        "owner_id": test_user['id']
-    }, {
-        "title": "2nd title",
-        "content": "2nd content",
-        "owner_id": test_user['id']
-    },
+    posts_data = [
         {
-        "title": "3rd title",
-        "content": "3rd content",
-        "owner_id": test_user['id']
-    }, {
-        "title": "3rd title",
-        "content": "3rd content",
-        "owner_id": test_user2['id']
-    }]
+            "title": "first title",
+            "content": "first content",
+            "user_id": test_user["id"],
+        },
+        {
+            "title": "2nd title",
+            "content": "2nd content",
+            "user_id": test_user["id"],
+        },
+        {
+            "title": "3rd title",
+            "content": "3rd content",
+            "user_id": test_user["id"],
+        },
+        {
+            "title": "3rd title",
+            "content": "3rd content",
+            "user_id": test_user2["id"],
+        },
+    ]
 
     def create_post_model(post):
         return models.Post(**post)
 
-    post_map = map(create_post_model, posts_data)
-    posts = list(post_map)
+    posts = list(map(create_post_model, posts_data))
 
     session.add_all(posts)
-
     session.commit()
 
-    posts = session.query(models.Post).all()
-    return posts
+    return session.query(models.Post).all()
